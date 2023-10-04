8fdbca9f485a9d87bfedf9d655f6579dd04b56fc
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-04 19:14:40 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,52 +1,39 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# sets up the web servers for the deployment of web_static
+# Sets up the web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
-=======
-# Sets up a web server for deployment of web_static.
+# Update package lists
+apt update
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx
+apt install -y nginx
 
+# - Create the folders, if they don't yet exist:
+#   * '/data'
+#   * '/data/web_static/'
+#   * '/data/web_static/releases/'
+#   * '/data/web_static/releases/test/'
+#   * '/data/web_static/shared/'
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file '/data/web_static/releases/test/index.html',
+# (with simple content, to test Nginx configuration)
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | 
+tee /data/web_static/releases/test/index.html 
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create a symbolic link '/data/web_static/current' linked to the
+# '/data/web_static/releases/test/' folder.
+ln -fs /data/web_static/releases/test/ /data/web_static/current
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+# Give recursive ownership of the '/data/' folder to the 'ubuntu' user AND group
+chown -R ubuntu:ubuntu /data/
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Update the Nginx configuration to serve the content of '/data/web_static/current/'
+# to 'hbnb_static' (ex: https://mydomainname.tech/hbnb_static).
+loc_header="location \/hbnb\_static\/ {"
+loc_content="alias \/data\/web\_static\/current\/;"
+new_location="\n\t$loc_header\n\t\t$loc_content\n\t}\n"
+sed -i "37s/$/$new_location/" /etc/nginx/sites-available/default
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
+# Restart Nginx
 service nginx restart
->>>>>>> 3be6b736b343528875751ac562c2e09313399515
